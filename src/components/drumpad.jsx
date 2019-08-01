df39cac391e604f241a8e9e05b127cac5dcc0ca9
Ray Mathew <ray10mathew@gmail.com>
2019-08-01 10:27:27 +0530

    added animation for pad click

--- src/components/drumpad.jsx
+++ src/components/drumpad.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 
 
 class DrumPad extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            clicked: false
+        };
+    }
 
     playAudio = () => {
         const audio = document.getElementById(this.props.id);
         audio.currentTime = 0;
         audio.play();
+        this.padClickAnimation();
+    };
+
+    padClickAnimation = () => {
+        this.setState({
+            clicked: true
+        }, () => {
+            setTimeout(() => {
+                this.setState({
+                    clicked: false
+                });
+            },100);
+        });
     };
 
     render () {
+        const classes = this.state.clicked? 'drum-pad clicked' : 'drum-pad';
         return (
-            <div className="drum-pad" onClick={this.playAudio}>
+            <div className={classes} onClick={this.playAudio}>
                 <audio src={this.props.clip} className="clip" id={this.props.id}></audio>
                 {this.props.id}
             </div>
